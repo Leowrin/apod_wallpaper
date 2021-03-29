a58f1c44a4ac842d3a58545490f36a47606f5637
Leowrin <leo.dana@epfl.ch>
2021-03-29 19:05:07 +0200

    .

--- apod_script/get_desktop_environment.py
+++ apod_script/get_desktop_environment.py
@@ -1,8 +1,8 @@
-#need to import os, sys in main?
+# need to import os, sys in main?
 import os, sys
 
 def get_desktop_environment():
-    #From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
+    # From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
     # and http://ubuntuforums.org/showthread.php?t=652320
     # and http://ubuntuforums.org/showthread.php?t=652320
     # and http://ubuntuforums.org/showthread.php?t=1139057
@@ -10,14 +10,14 @@ def get_desktop_environment():
         return "windows"
     elif sys.platform == "darwin":
         return "mac"
-    else: #Most likely either a POSIX system or something not much common
+    else: # Most likely either a POSIX system or something not much common
         desktop_session = os.environ.get("DESKTOP_SESSION")
         if desktop_session is not None: #easier to match if we doesn't have  to deal with caracter cases
             desktop_session = desktop_session.lower()
             if desktop_session in ["gnome","unity", "cinnamon", "mate", "xfce4", "lxde", "fluxbox",
                                    "blackbox", "openbox", "icewm", "jwm", "afterstep","trinity", "kde"]:
                 return desktop_session
-            ## Special cases ##
+            # Special cases ##
             # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
             # There is no guarantee that they will not do the same with the other desktop environments.
             elif "xfce" in desktop_session or desktop_session.startswith("xubuntu"):
